4610e9bc77e77fc6632d9b6e4046c569c8a44094
Yuriy Tyukalov <ramgml@gmail.com>
2020-02-18 10:33:50 +0400

    Third exercise

--- for.py
+++ for.py
@@ -9,13 +9,26 @@
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
+import string
+import random
 
-def main():
+
+def main(classes: list):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    sum_by_school = 0
+    for school_class in classes:
+        average_by_class = sum(school_class['scores']) / len(school_class['scores'])
+        print("Класс %s: %s)" % (school_class['school_class'], average_by_class))
+        sum_by_school += average_by_class
+    print('Средняя оценка по школе: ' + str(round(sum_by_school / len(classes), 1)))
+
+
 if __name__ == "__main__":
-    main()
+    school = [{
+        'school_class': str(random.randint(1, 11)) + random.choice(string.ascii_lowercase[:5]),
+        'scores': [random.randint(1, 5) for i in range(5)]
+    } for i in range(5)]
+    main(school)
